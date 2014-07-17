f7bc834cf55f0f2dda75e4a0aed618725bfbd1b8
gerhard2202 <tetzner.gerhard@gmail.com>
2014-07-17 14:16:32 -0400

    Recursion works

--- src/main/java/com/pawd/recursivepickaxe/item/ItemRecursivePickaxe.java
+++ src/main/java/com/pawd/recursivepickaxe/item/ItemRecursivePickaxe.java
@@ -77,13 +77,32 @@ public class ItemRecursivePickaxe extends ItemRPTool
                 if(!NBTHelper.getBoolean(stack,"mining"))
                 {
                     NBTHelper.setBoolean(stack, "mining", true);
-                    mplayer.theItemInWorldManager.tryHarvestBlock(x, y, z);
+                    mineVein(block,x,y,z,world.getBlockMetadata(x,y,z),world,mplayer);
                 }
                 NBTHelper.setBoolean(stack, "mining", false);
                 return false;
             }
         }
         return super.onBlockStartBreak(stack,x,y,z,player);
+    }
+
+    public void mineVein(Block block, int x, int y, int z, int meta, World world, EntityPlayerMP mplayer)
+    {
+        for(int x2 = x - 1; x2 <= x + 1; x2++)
+        {
+            for(int y2 = y - 1; y2 <= y + 1; y2++)
+            {
+                for(int z2 = z - 1; z2 <= z + 1; z2++)
+                {
+                    Block block2 = world.getBlock(x2,y2,z2);
+                    if(block2 == block & world.getBlockMetadata(x2,y2,z2) == meta)
+                    {
+                        mplayer.theItemInWorldManager.tryHarvestBlock(x2,y2,z2);
+                        mineVein(block,x2,y2,z2,meta,world,mplayer);
+                    }
+                }
+            }
+        }
     }
 
     @Override
