865a43e58b2ad7ae58093360dafb283987c7c1bd
gerhard2202 <tetzner.gerhard@gmail.com>
2014-07-17 02:59:42 -0400

    Got pickaxe functional; works as a standard pickaxe

--- src/main/java/com/pawd/recursivepickaxe/init/ModItems.java
+++ src/main/java/com/pawd/recursivepickaxe/init/ModItems.java
@@ -1,14 +1,16 @@
 package com.pawd.recursivepickaxe.init;
 
 import com.pawd.recursivepickaxe.item.ItemRP;
+import com.pawd.recursivepickaxe.item.ItemRPTool;
 import com.pawd.recursivepickaxe.item.ItemRecursivePickaxe;
 import com.pawd.recursivepickaxe.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
 
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModItems
 {
-    public static final ItemRP recursivePickaxe = new ItemRecursivePickaxe();
+    public static final ItemRPTool recursivePickaxe = new ItemRecursivePickaxe(Item.ToolMaterial.EMERALD);
 
     public static void init()
     {
--- src/main/java/com/pawd/recursivepickaxe/item/ItemRecursivePickaxe.java
+++ src/main/java/com/pawd/recursivepickaxe/item/ItemRecursivePickaxe.java
@@ -1,15 +1,62 @@
 package com.pawd.recursivepickaxe.item;
 
 import com.pawd.recursivepickaxe.creativetab.CreativeTabRP;
+import com.pawd.recursivepickaxe.reference.BlockReference;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 
-public class ItemRecursivePickaxe extends ItemRP
+public class ItemRecursivePickaxe extends ItemRPTool
 {
-    public ItemRecursivePickaxe()
+
+    public ItemRecursivePickaxe(ToolMaterial toolMaterial)
     {
-        super();
+        super(2, toolMaterial, BlockReference.BLOCKS_PICKAXE);
         this.setUnlocalizedName("recursivePickaxe");
-        this.setCreativeTab(CreativeTabRP.RP_TAB);
-        this.setMaxStackSize(1);
-        this.setHarvestLevel("pickaxe", 2);
+    }
+
+    @Override
+    public boolean canHarvestBlock(Block block, ItemStack stack)
+    {
+/* ========= MEKANISM STYLE ==========*/
+/*      int hardness = 0;
+        if(block == Blocks.obsidian)
+        {
+            hardness = 3;
+        }
+        if(block == Blocks.diamond_block || block == Blocks.diamond_ore
+           || block == Blocks.gold_block || block == Blocks.gold_ore
+           || block == Blocks.redstone_ore || block == Blocks.lit_redstone_ore)
+        {
+            hardness = 2;
+        }
+        if(block == Blocks.iron_block || block == Blocks.iron_ore
+           || block == Blocks.lapis_block || block == Blocks.lapis_ore)
+        {
+            hardness = 1;
+        }
+        if(hardness != 0)
+        {
+            return toolMaterial.getHarvestLevel() >= hardness;
+        }
+        else
+        {
+            if(block.getMaterial() == Material.rock) return true;
+        }
+
+        return block.getMaterial() == Material.iron; */
+
+/* ====== SIMPLE, HARDCODED STYLE ====== */
+        if(block.getMaterial() == Material.rock) return true;
+        return block.getMaterial() == Material.iron;
+
+    }
+    @Override
+    public float getDigSpeed(ItemStack stack, Block block, int meta)
+    {
+        if(block.getMaterial() == Material.rock) return 20f;
+        if(block.getMaterial() == Material.iron) return 20f;
+        return 4f;
     }
 }
